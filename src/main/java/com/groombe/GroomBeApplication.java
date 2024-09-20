a0b7da97586b00ecf107726774f61c8dd2e09563
EunbeenDev <ros4312@naver.com>
2024-09-20 12:01:19 +0900

    initial commit

---
+++ src/main/java/com/groombe/GroomBeApplication.java
@@ -0,0 +1,13 @@
+package com.groombe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroomBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroomBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/groombe/GroomBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groombe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroomBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
