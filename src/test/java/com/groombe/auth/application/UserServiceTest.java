688f4a3f91a335de986001ff44c6b0732d09fb46
EunbeenDev <ros4312@naver.com>
2024-09-20 13:31:42 +0900

    [FEAT] 회원가입 기능 구현

--- src/test/java/com/groombe/auth/application/UserServiceTest.java
+++ src/test/java/com/groombe/auth/application/UserServiceTest.java
@@ -1,4 +1,53 @@
 package com.groombe.auth.application;
 
+import com.groombe.auth.domain.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    private User student;
+    private User admin;
+
+    @BeforeEach
+    public void setUp() {
+        student = new User("student1", "password123", "STUDENT");
+        admin = new User("admin1", "adminpass", "ADMIN");
+    }
+
+    @Test
+    public void testStudentRegistration() {
+        User registeredStudent = userService.registerUser(student);
+        assertNotNull(registeredStudent);
+        assertEquals("student1", registeredStudent.getUsername());
+    }
+
+    @Test
+    public void testAdminRegistration() {
+        User registeredAdmin = userService.registerUser(admin);
+        assertNotNull(registeredAdmin);
+        assertEquals("admin1", registeredAdmin.getUsername());
+    }
+
+    @Test
+    public void testUserLoginSuccess() {
+        userService.registerUser(student);
+        User loggedInUser = userService.login("student1", "password123");
+        assertNotNull(loggedInUser);
+        assertEquals("student1", loggedInUser.getUsername());
+    }
+
+    @Test
+    public void testUserLoginFailure() {
+        User loggedInUser = userService.login("nonexistent", "wrongpassword");
+        assertNull(loggedInUser);
+    }
 }
